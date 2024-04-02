524494c530d97d43bd467569d4ac4065e3c7ebb1
Bryce Cole <brycewcole@gmail.com>
2024-04-01 21:40:43 -0400

    adding tab back

--- wezterm/.config/wezterm/appearance.lua
+++ wezterm/.config/wezterm/appearance.lua
@@ -4,7 +4,7 @@ local module = {}
 function module.apply_to_config(config)
 	config.color_scheme = "nord"
 	config.window_decorations = "RESIZE"
-	config.enable_tab_bar = false
+	config.hide_tab_bar_if_only_one_tab = true
 	config.font = wezterm.font({ family = "FiraCode Nerd Font", harfbuzz_features = { "calt=1", "clig=0", "liga=0" } })
 
 	config.font_size = 15.0
