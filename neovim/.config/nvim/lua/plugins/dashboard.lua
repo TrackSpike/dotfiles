495763edcb7274184d7ec609999a56698f2e1ca7
Bryce Cole <brycewcole@gmail.com>
2024-04-01 21:22:13 -0400

    change quote

--- neovim/.config/nvim/lua/plugins/dashboard.lua
+++ neovim/.config/nvim/lua/plugins/dashboard.lua
@@ -61,7 +61,7 @@ return {
           },
         },
         footer = function()
-          return { "A craftsman must sharpen his tools to do his job" }
+          return { "Sharp tools make good work." }
         end,
       },
     })
