d64870fcc36e5b71edda19616a503ac42ef58697
Bryce Cole <brycewcole@gmail.com>
2024-07-01 18:24:31 -0400

    Adding group index

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -803,9 +803,9 @@ require('lazy').setup({
         },
         sources = {
           { name = 'copilot', group_index = 2 },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
+          { name = 'nvim_lsp', group_index = 2 },
+          { name = 'luasnip', group_index = 2 },
+          { name = 'path', group_index = 2 },
         },
       }
     end,
